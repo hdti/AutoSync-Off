dda60d3be49685b28d45200af8b106c0b22ee4eb
dominique.hereish@cgi.com <dominique.hereish@cgi.com>
2025-08-09 13:29:52 -0400

    init script

--- environements/dev/clusters/minikube/init.sh
+++ environements/dev/clusters/minikube/init.sh
@@ -1,1 +1,30 @@
-kustomize build --enable-helm --enable-alpha-plugins --load-restrictor=LoadRestrictionsNone .
+#!/bin/bash
+
+# Install argocd
+kustomize build --enable-helm --enable-alpha-plugins --load-restrictor=LoadRestrictionsNone ../../resources/addons/argocd/. \
+| kubectl apply -f -
+# Wait for argocd to be ready
+echo "Waiting for argocd to be ready..."
+
+while true; do
+  STATUS=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server \
+  -o jsonpath='{.items[0].status.phase}')
+  if [ "$STATUS" == "Running" ]; then
+    echo "ArgoCD server pod ready"
+    break
+  fi
+  echo "Current Status: $STATUS. Waiting..."
+  sleep 5
+done
+# Get argocd admin password
+echo 
+echo "Getting ArgoCD admin password..."
+ADMIN_PW=$(kubectl get secret argocd-initial-admin-secret -n argocd \
+-o jsonpath="{.data.password}" | base64 -d)
+echo "=========================="
+echo "ArgoCD Admin Password: $ADMIN_PW"
+echo "=========================="
+
+# Install all apps and addons and making argocd manage itself
+kustomize build --enable-helm --enable-alpha-plugins --load-restrictor=LoadRestrictionsNone . \
+| kubectl apply -f -
